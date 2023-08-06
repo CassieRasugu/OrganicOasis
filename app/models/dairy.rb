002e8534c5060acee6fb1b52010d2014e53b0bb5
Cassie <cassierasugu@gmail.com>
2023-08-06 21:21:40 +0300

    changes

--- app/models/dairy.rb
+++ app/models/dairy.rb
@@ -2,6 +2,5 @@ class Dairy < ApplicationRecord
   belongs_to :category
   belongs_to :user, foreign_key: :user_id
   has_many :order_items
- has_many :reviews,dependent: :destroy
   has_many :orders, through: :order_items
 end
