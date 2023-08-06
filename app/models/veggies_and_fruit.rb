96b964c5cb50a04c55235a89b9dc3f2ee945fbfe
Cassie <cassierasugu@gmail.com>
2023-08-06 21:17:48 +0300

    changes

--- app/models/veggies_and_fruit.rb
+++ app/models/veggies_and_fruit.rb
@@ -1,2 +1,7 @@
 class VeggiesAndFruit < ApplicationRecord
+  belongs_to :category
+  belongs_to :user, foreign_key: :user_id
+  has_many :order_items
+ has_many :reviews,dependent: :destroy
+  has_many :orders, through: :order_items
 end
