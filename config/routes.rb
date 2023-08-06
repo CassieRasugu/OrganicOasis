4f53bc6d1bab5be7160fc73858c863ac6c8ca6a9
Cassie <cassierasugu@gmail.com>
2023-08-06 15:36:23 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    get '/meat', to: 'meat#index'
+    get '/beverage', to: 'beverage#index'
+    get '/bakery', to: 'bakery#index'
+    get '/spices', to: 'spices#index'
+    get '/cereal', to: 'cereal#index'
+    get '/dairy', to: 'dairy#index'
+    get '/veggies_and_fruits', to: 'veggies_and_fruits#index'
+    get '/offers', to: 'offers#index'
+  end
 end
